02ee4a14cef5494757691f596455bfb923f4c620
valentinogagliardi <valentino@valentinog.com>
2022-07-27 10:07:41 +0200

    Apply styles to checkbox

--- src/common/Checkbox.tsx
+++ src/common/Checkbox.tsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import styled from 'styled-components'
+
+import { CheckIcon } from '@radix-ui/react-icons'
+
 import {
   Checkbox as CheckboxPrimitive,
   Indicator as IndicatorPrimitive
@@ -12,15 +16,43 @@ import type {
 type Props = {
   rootProps?: CheckboxProps
   indicatorProps?: CheckboxIndicatorProps
-  children: React.ReactNode
+  children?: React.ReactNode
 }
 
 const Checkbox = ({ rootProps, indicatorProps, children }: Props) => {
   return (
-    <CheckboxPrimitive {...rootProps}>
-      <IndicatorPrimitive {...indicatorProps}>{children}</IndicatorPrimitive>
-    </CheckboxPrimitive>
+    <StyledCheckbox {...rootProps}>
+      <StyledIndicator {...indicatorProps}>
+        {children || <CheckIcon />}
+      </StyledIndicator>
+    </StyledCheckbox>
   )
 }
+
+const StyledCheckbox = styled(CheckboxPrimitive)`
+  all: unset;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 16px;
+  height: 16px;
+  background-color: #ffffff;
+  border: 1px solid #e6e9f2;
+  border-radius: 2px;
+
+  &[data-state='checked'] {
+    background-color: #25408f;
+  }
+`
+
+const StyledIndicator = styled(IndicatorPrimitive)`
+  all: unset;
+  width: 16px;
+  height: 16px;
+
+  svg {
+    color: #ffffff;
+  }
+`
 
 export { Checkbox }
